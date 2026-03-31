d2944162dc5d321aa2dd8c759cd601ad289ee85d
PerfZero <gundenis1@gmail.com>
2026-03-31 15:59:34 +0300

    chore(cms): expose menu-services read endpoints publicly

--- apps/cms/src/api/menu-service/routes/menu-service.ts
+++ apps/cms/src/api/menu-service/routes/menu-service.ts
@@ -2,6 +2,15 @@
  * menu-service router
  */
 
-import { factories } from '@strapi/strapi';
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreRouter('api::menu-service.menu-service');
+export default factories.createCoreRouter("api::menu-service.menu-service", {
+  config: {
+    find: {
+      auth: false,
+    },
+    findOne: {
+      auth: false,
+    },
+  },
+});
