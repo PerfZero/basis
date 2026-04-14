c45b12b8e60afdadb7d729ce43b4c0cece4f5ef2
PerfZero <gundenis1@gmail.com>
2026-04-14 13:52:39 +0300

    feat(cms): enable CKEditor custom field for company documents

--- apps/cms/config/middlewares.ts
+++ apps/cms/config/middlewares.ts
@@ -3,7 +3,21 @@ import type { Core } from '@strapi/strapi';
 const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'script-src': ["'self'", 'https:', 'cdn.ckeditor.com'],
+          'connect-src': ["'self'", 'https:', 'proxy-event.ckeditor.com'],
+          'img-src': ["'self'", 'data:', 'blob:', 'cdn.ckeditor.com', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
